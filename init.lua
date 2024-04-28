719f2df6e171fa9fc9d620d44bcb8fcf24511335
ghosert <ghosert@gmail.com>
2024-04-28 14:09:56 -0700

    open friendly-snippets for all language

--- init.lua
+++ init.lua
@@ -713,12 +713,12 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          { -- jiawzhang: uncommented this for auto complete snippet with vscode style.
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
---
+++ sample/test.lua
@@ -0,0 +1,5 @@
+function hello(s, j)
+  print('Hello ' .. s .. ' and ' .. j .. '!')
+end
+
+hello('Lua', 'World') -- neovim has auto complete feature on this method with this parameters
