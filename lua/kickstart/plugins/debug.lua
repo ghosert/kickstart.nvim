696416bdf602d4bf80511246df737182c646f7be
ghosert <ghosert@gmail.com>
2024-05-30 06:02:37 +0000

    refine debug.lua

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -32,6 +32,37 @@ return {
       'microsoft/vscode-js-debug',
       -- After install, build it and rename the dist directory to out
       build = 'npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out',
+      config = function()
+        -- jiawzhang Install javascript specific config: https://github.com/mxsdev/nvim-dap-vscode-js
+        require('dap-vscode-js').setup {
+          -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+          -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
+          debugger_path = vim.fn.resolve(vim.fn.stdpath 'data' .. '/lazy/vscode-js-debug'),
+          -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+          adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+          -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+          -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+          -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+        }
+        for _, language in ipairs { 'typescript', 'javascript' } do
+          require('dap').configurations[language] = {
+            {
+              type = 'pwa-node',
+              request = 'launch',
+              name = 'Launch file',
+              program = '${file}',
+              cwd = '${workspaceFolder}',
+            },
+            {
+              type = 'pwa-node',
+              request = 'attach',
+              name = 'Attach',
+              processId = require('dap.utils').pick_process,
+              cwd = '${workspaceFolder}',
+            },
+          }
+        end
+      end,
     },
     {
       'mfussenegger/nvim-dap-python', -- jiawzhang add for python debug, relying on 'pip2 install debugpy' under virtual env ~/devenv, preinstalled.
@@ -104,35 +135,5 @@ return {
 
     -- Install golang specific config
     -- require('dap-go').setup() -- jiawzhang comment it out since we don't have go language here.
-
-    -- jiawzhang Install javascript specific config: https://github.com/mxsdev/nvim-dap-vscode-js
-    require('dap-vscode-js').setup {
-      -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-      -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-      debugger_path = vim.fn.resolve(vim.fn.stdpath 'data' .. '/lazy/vscode-js-debug'),
-      -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-      adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
-      -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-      -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-      -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-    }
-    for _, language in ipairs { 'typescript', 'javascript' } do
-      require('dap').configurations[language] = {
-        {
-          type = 'pwa-node',
-          request = 'launch',
-          name = 'Launch file',
-          program = '${file}',
-          cwd = '${workspaceFolder}',
-        },
-        {
-          type = 'pwa-node',
-          request = 'attach',
-          name = 'Attach',
-          processId = require('dap.utils').pick_process,
-          cwd = '${workspaceFolder}',
-        },
-      }
-    end
   end,
 }
