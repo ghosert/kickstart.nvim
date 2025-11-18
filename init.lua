5729a84be30be0fc5e42474406092df1c20ea1ae
ghosert <ghosert@gmail.com>
2025-11-18 03:41:36 +0000

    claudecode nvim

--- init.lua
+++ init.lua
@@ -1235,6 +1235,37 @@ require('lazy').setup({
       require('alpha').setup(dashboard.opts)
     end,
   },
+  {
+    'coder/claudecode.nvim',
+    dependencies = {
+      'folke/snacks.nvim',
+      -- jiawzhang NOTE: adding keys to move cursor between claude code pane and neovim pane.
+      keys = {
+        { '<C-h>', '<cmd>TmuxNavigateLeft<cr>', mode = 't' },
+        { '<C-l>', '<cmd>TmuxNavigateRight<cr>', mode = 't' },
+      },
+    },
+    config = true,
+    keys = {
+      { '<leader>a', nil, desc = 'AI/Claude Code' },
+      { '<leader>ac', '<cmd>ClaudeCode<cr>', desc = 'Toggle Claude' },
+      { '<leader>af', '<cmd>ClaudeCodeFocus<cr>', desc = 'Focus Claude' },
+      { '<leader>ar', '<cmd>ClaudeCode --resume<cr>', desc = 'Resume Claude' },
+      { '<leader>aC', '<cmd>ClaudeCode --continue<cr>', desc = 'Continue Claude' },
+      { '<leader>am', '<cmd>ClaudeCodeSelectModel<cr>', desc = 'Select Claude model' },
+      { '<leader>ab', '<cmd>ClaudeCodeAdd %<cr>', desc = 'Add current buffer' },
+      { '<leader>as', '<cmd>ClaudeCodeSend<cr>', mode = 'v', desc = 'Send to Claude' },
+      {
+        '<leader>as',
+        '<cmd>ClaudeCodeTreeAdd<cr>',
+        desc = 'Add file',
+        ft = { 'NvimTree', 'neo-tree', 'oil', 'minifiles', 'netrw' },
+      },
+      -- Diff management
+      { '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>', desc = 'Accept diff' },
+      { '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', desc = 'Deny diff' },
+    },
+  },
   { -- jiawzhang, add for use ctrl + hjkl to move focus from neovim to another tmux window.
     'christoomey/vim-tmux-navigator',
     vim.keymap.set('n', '<C-h>', ':TmuxNavigateLeft<CR>'),
